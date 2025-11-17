69aa264f1bfc807f7391431ea5fb251df51346fa
johnsijoy <johnsijoy9@gmail.com>
2025-11-17 10:02:05 +0530

    Updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 // src/api/axios.js
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api/";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||  "https://smart-property-locator-backend-2.onrender.com/api/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
